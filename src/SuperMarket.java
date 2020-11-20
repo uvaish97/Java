48fd2b1098d1f5ce8e901287a5bd07146d25b2d2
uvaish97 <uvaish1997@gmail.com>
2020-11-20 12:22:14 +0530

    shop

---
+++ src/SuperMarket.java
@@ -0,0 +1,28 @@
+
+public class SuperMarket extends Vasanthstores {
+	@Override
+	public void goldwinneroil() {
+		// TODO Auto-generated method stub
+		System.out.println("price = 90");
+	}
+	@Override
+	public void gingerPaste() {
+		// TODO Auto-generated method stub
+		System.out.println("price = 30");
+		
+	}
+	@Override
+	public void noodles() {
+		// TODO Auto-generated method stub
+		System.out.println("price = 20");
+	}
+public static void main(String[] args) {
+	SuperMarket s = new SuperMarket();
+	s.goldwinneroil();
+	s.gingerPaste();
+	s.noodles();
+	
+}
+	}
+
+
---
+++ src/Vasanthstores.java
@@ -0,0 +1,17 @@
+
+public class Vasanthstores {
+public void goldwinneroil() {
+	// TODO Auto-generated method stub
+	System.out.println("price = 90");
+	}
+public void gingerPaste() {
+	// TODO Auto-generated method stub
+	System.out.println("price = 30");
+
+}
+public void noodles() {
+	// TODO Auto-generated method stub
+	System.out.println("price = 15");
+
+}
+}
